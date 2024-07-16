b39b8d837e466fa345f0b7a776067853deba98a5
hottabych04 <hottabych04@gmail.com>
2024-07-16 12:29:43 +0300

    Init commit

---
+++ src/main/java/com/hottabych04/app/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.hottabych04.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class);
+    }
+}
