b83c1acb19b19974bb9d59d1c7c5eb568040decb
hottabych04 <hottabych04@gmail.com>
2024-07-18 18:14:03 +0300

    Add new changelog

---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+
+--changeset hottabych04:1
+INSERT INTO file (title, description, creation_date, data)
+VALUES
+    ('testFile1', 'description1', '2024-07-17 21:36:17.000000', 'pofopgmdfsimgoismdo[gmoim43k09tg230jyt0i4nrogn0m025493,mg0452jh09m20hm540mh02j59h0m254mh'),
+    ('testFile2', 'description2', '2020-09-17 21:12:41.000000', 'o2m5i04yni025n0m=01254-=0gkj1jg04j130gjk1o0mg[oi34mjtmjm10gm0gm-01mg0'),
+    ('testFile3', 'description3', '2014-05-22 15:44:47.000000', 't1034jn0j1309mfo3mfpl,3le,fo,341k09kg=01mgiomniogm1=034mgp1mgm3edemfg3490mf'),
+    ('testFile4', 'description4', '1999-01-30 23:37:12.000000', 't1340jgm4pokmok1mfme3090f1k3409fkgl4kmf1okmmkodmflkmdam90rkgf0-m2img'),
+    ('testFile5', 'description5', '2023-12-01 17:35:22.000000', '134pjf4903mfklmd,asmgkfsdmg0a0-r9gi-90j29nt54kmf0rk90fgserkgw09merokgm9i2mt'),
+    ('testFile6', 'description6', '2019-10-07 23:38:50.000000', '14189tru49jhgfndalgm kfdmgmasg90290jgifkjasngklcdmsmvopmwdfop[okgv90kw09erigj245gdklanmsdf'),
+    ('testFile7', 'description7', '2005-10-07 11:35:45.000000', '1t48hj8jrajonfgkdjasnvkjbnkjfsdanvgnmefokmglkwejrkgjeiojg89p952hjp98htrkjngskjlnfgkjnfsdjnguinas89fghj982j45nkj26nkj245ni4fjg9s8ahj'),
+    ('testFile8', 'description8', '2024-03-15 21:42:26.000000', 'ag9h42jntlgknr2i4igmkmfsdklmgldfsngbnfbvcujb89xcvvub89b39n5jnj5n2jn43tpfsigf'),
+    ('testFile9', 'description9', '2022-07-24 15:46:36.000000', 'gwgjij245lklknlknlkfmskgmfdksmbfdsbkosio-civis9dcv9ucoisjbkbznvchoip2-u1uioajgnrege'),
+    ('testFile10', 'description10', '2024-11-22 14:32:16.000000', '290ut5n2kjn4km5ntnrwokrfmgsdkfkgdfbdsnbgfnfg8n09gfd9-gn=25=2i6]456p4525o[pjokjkgntjnguiwrtbug'),
+    ('testFile11', 'description11', '2009-10-11 10:50:48.000000', 'rew90ug90rjogijrknwkmrkmgmffb90cvx0b890vc8xbuioj254knmlkmwkmtkornimwkpfokgpo[xf9ig0ue'),
+    ('testFile12', 'description12', '2000-07-24 14:58:20.000000', '24562905uj90jroifmglksmfdlkmgkm35409yhkpowklwpk'';lwelmwkrnywnkjgnwflkjngifxucbx7cvb9i20495ikypoglkenmrtglkjwnkjnbrgw'),
+    ('testFile13', 'description13', '2013-03-31 21:45:32.000000', 'gwer-90gk90iwkf0kgowjglkmknnvzvkbmncvxmbcxvbkpxcovkbkjxcvoibop[i[0oriet-9we0rti09u24j23nktjnrknnmgjfoijsdgf'),
+    ('testFile14', 'description14', '2024-07-01 21:45:54.000000', '90dfusbub90-840596oi2hi6hvuvugvadbafdoapoqp[o[po[pkpefkldnnmvb''v;cxlb'';lvcp''bop[ore-w09rt02849806781438914u3h1jierkjnjnrjwngjfnkjnvkjhfkjhjfknsajfg'),
+    ('testFile15', 'description15', '2022-09-12 08:39:12.000000', 'cvkndfkjhnvgiun98341nt091jt039erkf09-daigfdag[asg]34qk]ptpkgpokrgfpokqjp[odfgj0q9jrg980qj9hjguijbn134ubg4h8g98h');
